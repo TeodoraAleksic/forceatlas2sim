674c9e9a1c9443d21b0b3df1b5988e85e53a52d3
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-06-11 17:05:27 +0200

    File parser class

---
+++ forceatlas2sim/fileparser.cpp
@@ -0,0 +1,9 @@
+#include "fileparser.h"
+
+FileParser::FileParser(std::string fileName)
+{
+}
+
+FileParser::~FileParser()
+{
+}
---
+++ forceatlas2sim/fileparser.h
@@ -0,0 +1,15 @@
+#ifndef _FILEPARSER_H_
+#define _FILEPARSER_H_
+
+#include <string>
+
+class FileParser
+{
+public:
+
+	FileParser(std::string fileNae);
+	~FileParser();
+
+};
+
+#endif

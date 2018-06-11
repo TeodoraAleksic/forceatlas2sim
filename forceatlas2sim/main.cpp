02cfc16cb43ab4d72967742226f386487a1f6fb8
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-06-11 16:39:31 +0200

    Usage

--- forceatlas2sim/main.cpp
+++ forceatlas2sim/main.cpp
@@ -1,4 +1,38 @@
-int main()
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
 {
+	std::string help = "Usage: forceatlas2sim.exe (-i INPUT) [-h]\n\n"\
+		"GPU-based parallelization and graphical simulation of the ForceAtlas2 algorithm\n\n"\
+		"Options:\n"\
+		"-i INPUT   Input GEFX or GML graph file. Required.\n"\
+		"-h         Prints usage.\n";
+
+	std::string inputFile;
+
+	switch (argc)
+	{
+	case 1:
+		std::cout << help;
+		return 0;
+	case 2:
+		if (argv[1] == std::string("-h"))
+		{
+			std::cout << help;
+			return 0;
+		}
+	case 3:
+		if (argv[1] == std::string("-i"))
+		{
+			inputFile = argv[2];
+			break;
+		}
+	default:
+		std::cout << "Invalid arguments. ";
+		std::cout << help;
+		return 1;
+	}
+
 	return 0;
 }

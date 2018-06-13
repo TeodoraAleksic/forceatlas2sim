a723a52fdf95748f62b14b7ad6481b5a40c7ccae
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-06-13 15:30:33 +0200

    Setup OpenGL

--- forceatlas2sim/main.cpp
+++ forceatlas2sim/main.cpp
@@ -1,8 +1,15 @@
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+#include <GLFW\glfw3native.h>
+
 #include <iostream>
 #include <string>
 
 #include "graphobject.h"
 #include "fileparser.h"
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HIGHT = 600;
 
 int main(int argc, char** argv)
 {
@@ -39,6 +46,46 @@ int main(int argc, char** argv)
 
 	FileParser fileParser;
 	GraphObject graphObject = fileParser.parse(inputFile);
+
+	// Sets up OpenGL
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HIGHT, "ForceAtlas2Sim", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// Addresses OS specific OpenGL functions
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	// Rendering loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 
 	return 0;
 }

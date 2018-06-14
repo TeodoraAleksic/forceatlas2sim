54921a222aa63cd7523a467b0b55531c36654ba9
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-06-14 15:57:25 +0200

    Base GL class

---
+++ forceatlas2sim/globject.cpp
@@ -0,0 +1,82 @@
+#include "globject.h"
+
+#include <iostream>
+
+#include "utility.h"
+
+GLObject::GLObject()
+{
+}
+
+GLObject::~GLObject()
+{
+}
+
+std::string GLObject::getShaderName(GLenum shaderType)
+{
+	switch (shaderType)
+	{
+	case GL_COMPUTE_SHADER:
+		return "GL_COMPUTE_SHADER";
+	case GL_GEOMETRY_SHADER:
+		return "GL_GEOMETRY_SHADER";
+	case GL_FRAGMENT_SHADER:
+		return "GL_FRAGMENT_SHADER";
+	case GL_TESS_CONTROL_SHADER:
+		return "GL_TESS_CONTROL_SHADER";
+	case GL_TESS_EVALUATION_SHADER:
+		return "GL_TESS_EVALUATION_SHADER";
+	case GL_VERTEX_SHADER:
+		return "GL_VERTEX_SHADER";
+	default:
+		return "UNKNOWN";
+	}
+}
+
+unsigned int GLObject::buildShader(std::string filePath, GLenum shaderType)
+{
+	unsigned int shader;
+	shader = glCreateShader(shaderType);
+
+	// Builds shader from source
+	const char* shaderSource = readFile(filePath).c_str();
+	glShaderSource(shader, 1, &shaderSource, nullptr);
+	glCompileShader(shader);
+
+	// Gets build status
+	int success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		char infoLog[512];
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::" << getShaderName(shaderType) << "::COMPILATION_FAILED\n" << infoLog << "\n";
+	}
+
+	return shader;
+}
+
+unsigned int GLObject::buildProgram(std::vector<unsigned int> shaders)
+{
+	unsigned int program;
+	program = glCreateProgram();
+
+	for (auto iter = shaders.begin(); iter != shaders.end(); ++iter)
+		glAttachShader(program, *iter);
+
+	glLinkProgram(program);
+
+	// Gets build status
+	int success;
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+	if (!success) 
+	{
+		char infoLog[512];
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << "\n";
+	}
+
+	return program;
+}
---
+++ forceatlas2sim/globject.h
@@ -0,0 +1,30 @@
+#ifndef _GLOBJECT_H_
+#define _GLOBJECT_H_
+
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+#include <string>
+#include <vector>
+
+class GLObject
+{
+protected:
+
+	std::string getShaderName(GLenum shaderType);
+
+	unsigned int buildShader(std::string fileName, GLenum shaderType);
+	unsigned int buildProgram(std::vector<unsigned int> shaders);
+
+public:
+
+	GLObject();
+	~GLObject();
+
+	virtual void init() = 0;
+	virtual void draw() = 0;
+	virtual void cleanup() = 0;
+
+};
+
+#endif

400f1d4f28d35110bf90852e4edea392606aa6fa
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-06-30 15:42:07 +0200

    Remove redundant functions from CLUpdateEdge

--- forceatlas2sim/clupdateedge.h
+++ forceatlas2sim/clupdateedge.h
@@ -9,26 +9,11 @@
 
 class CLUpdateEdge : public CLObject
 {
-private:
-
-	bool isInited, isSet;
-
-	std::vector<cl::Memory> glBuffers;
-	cl::BufferGL nodeX, nodeY, nodeZ;
-	cl::BufferGL sourceX, sourceY, sourceZ;
-	cl::BufferGL targetX, targetY, targetZ;
-
-	cl::Buffer sourceId, targetId;
-
 public:
 
-	CLUpdateEdge();
+	CLUpdateEdge(const cl::Device& device_, const cl::Context& context_);
 	~CLUpdateEdge();
 
-	void init();
-	void run();
-
-	void setArguments(const GraphObject& graphObject, const GLGraphNode& graphNode, const GLGraphEdge& graphEdge);
 };
 
 #endif

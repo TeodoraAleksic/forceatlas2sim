f37d8964e450a9615e182ffb9016c9e9cb73f1ae
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-07-14 18:06:58 +0200

    Guards for ForceAtlas2 parameter setters

--- forceatlas2sim/forceatlas2params.cpp
+++ forceatlas2sim/forceatlas2params.cpp
@@ -74,42 +74,66 @@ void ForceAtlas2Params::setInput(std::string input_)
 
 void ForceAtlas2Params::setKr(float kr_)
 {
+	if (kr_ < 0.0f)
+		throw "Repulsion force coefficient must be greater than 0.0";
+
 	kr = kr_;
 }
 
 void ForceAtlas2Params::setKr(std::string kr_)
 {
 	kr = stringToFloat(kr_);
+
+	if (kr < 0.0f)
+		throw "Repulsion force coefficient must be greater than 0.0";
 }
 
 void ForceAtlas2Params::setKrp(float krp_)
 {
+	if (krp_ < 0.0f)
+		throw "Repulsion force overlap coefficient must be greater than 0.0";
+
 	krp = krp_;
 }
 
 void ForceAtlas2Params::setKrp(std::string krp_)
 {
 	krp = stringToFloat(krp_);
+
+	if (krp < 0.0f)
+		throw "Repulsion force overlap coefficient must be greater than 0.0";
 }
 
 void ForceAtlas2Params::setFg(bool fg_)
 {
+	if (fsg && fg_)
+		throw "Can't set both normal and strong gravity";
+
 	fg = fg_;
 }
 
 void ForceAtlas2Params::setFsg(bool fsg_)
 {
+	if (fg && fsg_)
+		throw "Can't set both normal and strong gravity";
+
 	fsg = fsg_;
 }
 
 void ForceAtlas2Params::setKg(float kg_)
 {
+	if (kg_ < 0.0f)
+		throw "Gravitational force coefficient must be greater than 0.0";
+
 	kg = kg_;
 }
 
 void ForceAtlas2Params::setKg(std::string kg_)
 {
 	kg = stringToFloat(kg_);
+
+	if (kg < 0.0f)
+		throw "Gravitational force coefficient must be greater than 0.0";
 }
 
 void ForceAtlas2Params::setNw(bool nw_)

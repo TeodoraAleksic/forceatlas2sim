2b43bd2e23e4e45d87fd11e1c9d74277676a6690
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-07-17 21:39:37 +0200

    Fix calculating the attraction and repulsion forces

--- forceatlas2sim/kernel.h
+++ forceatlas2sim/kernel.h
@@ -6,66 +6,74 @@
 namespace kernel
 {
 	const std::string nBody =
-		" \
-		float dist(float x1, float y1, float z1, float x2, float y2, float z2) \
-		{ \
-			return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2)); \
-		} \
-		\
-		float size(uint degree) \
-		{ \
-			return degree * 0.5 + 0.5; \
-		} \
-		\
-		__kernel void nBody( \
-			__const uint n, \
-			__const float kr, \
-			__const float krp, \
-			__global float* x, \
-			__global float* y, \
-			__global float* z, \
-			__global uint* degree, \
-			__global float* fx, \
-			__global float* fy, \
-			__global float* fz) \
-		{ \
-			uint id = get_global_id(0); \
-			\
-			if (id < n) \
-			{ \
-				fx[id] = 0; \
-				fy[id] = 0; \
-				fz[id] = 0; \
-				\
-				uint i = (id + 1) < n ? (id + 1) : 0; \
-				\
-				while (i != id) \
-				{ \
-					float d = dist(x[id], y[id], z[id], x[i], y[i], z[i]) - size(degree[id]) - size(degree[i]); \
-					\
-					float degrees = (degree[id] + 1) * (degree[i] + 1); \
-					\
-					if (d > 0) \
-					{ \
-						fx[id] += (x[i] - x[id]) / (degree[id] + 1); \
-						fy[id] += (y[i] - y[id]) / (degree[id] + 1); \
-						fz[id] += (z[i] - z[id]) / (degree[id] + 1); \
-						\
-						fx[id] += kr * degrees / (x[id] - x[i]); \
-						fy[id] += kr * degrees / (y[id] - y[i]); \
-						fz[id] += kr * degrees / (z[id] - z[i]); \
-					} \
-					else if (d < 0) \
-					{ \
-						fx[id] += krp * degrees * ((x[id] - x[i]) > 0 ? 1 : -1); \
-						fy[id] += krp * degrees * ((y[id] - y[i]) > 0 ? 1 : -1); \
-						fz[id] += krp * degrees * ((z[id] - z[i]) > 0 ? 1 : -1); \
-					} \
-					\
-					i = (i + 1) < n ? (i + 1) : 0; \
-				} \
-			} \
-		} \
+		" \n\
+		float size(uint degree) \n\
+		{ \n\
+			return degree * 0.5 + 0.5; \n\
+		} \n\
+		\n\
+		float dist(float p1, float p2, uint degree1, uint degree2) \n\
+		{ \n\
+			return fabs(p1 - p2) - size(degree1) - size(degree2); \n\
+		} \n\
+		\n\
+		float fa(float p1, float p2, uint degree1, uint degree2) \n\
+		{ \n\
+			if (dist(p1, p2, degree1, degree2) > 0) \n\
+				return (p2 - p1) / (degree1 + 1); \n\
+			else \n\
+				return 0; \n\
+		} \n\
+		\n\
+		float fr(__const float kr, __const float kpr, float p1, float p2, uint degree1, uint degree2) \n\
+		{ \n\
+			float d = dist(p1, p2, degree1, degree2); \n\
+		\n\
+			if (d > 0) \n\
+				return kr * (degree1 + 1) * (degree2 + 1) / (p1 - p2); \n\
+			else if (d < 0) \n\
+				return kpr * (degree1 + 1) * (degree2 + 1) * ((p1 - p2) >= 0 ? 1 : -1); \n\
+			else \n\
+				return 0; \n\
+		} \n\
+		\n\
+		__kernel void nBody( \n\
+			__const uint n, \n\
+			__const float kr, \n\
+			__const float krp, \n\
+			__global float* x, \n\
+			__global float* y, \n\
+			__global float* z, \n\
+			__global uint* degree, \n\
+			__global float* fx, \n\
+			__global float* fy, \n\
+			__global float* fz) \n\
+		{ \n\
+			uint id = get_global_id(0); \n\
+			\n\
+			if (id < n) \n\
+			{ \n\
+				fx[id] = 0; \n\
+				fy[id] = 0; \n\
+				fz[id] = 0; \n\
+				\n\
+				uint i = (id + 1) < n ? (id + 1) : 0; \n\
+				\n\
+				while (i != id) \n\
+				{ \n\
+					fx[id] += fa(x[id], x[i], degree[id], degree[i]); \n\
+					fx[id] += fr(kr, krp, x[id], x[i], degree[id], degree[i]); \n\
+					\n\
+					fy[id] += fa(y[id], y[i], degree[id], degree[i]); \n\
+					fy[id] += fr(kr, krp, y[id], y[i], degree[id], degree[i]); \n\
+					\n\
+					fz[id] += fa(z[id], z[i], degree[id], degree[i]); \n\
+					fz[id] += fr(kr, krp, z[id], z[i], degree[id], degree[i]); \n\
+					\n\
+					i = (i + 1) < n ? (i + 1) : 0; \n\
+				} \n\
+			} \n\
+		} \n\
 		";
 
 	const std::string updateNode =

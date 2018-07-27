46341b0aecfc4bc81b45131bd4c1e08904cf6c47
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-07-27 20:51:33 +0200

    Refactor initializing OpenCL context

--- forceatlas2sim/clcontext.cpp
+++ forceatlas2sim/clcontext.cpp
@@ -16,34 +16,43 @@ CLContext::~CLContext()
 
 void CLContext::init()
 {
+	// Gets number of available OpenCL platforms
 	unsigned int numOfPlatforms;
 	clGetPlatformIDs(0, nullptr, &numOfPlatforms);
 
-	std::vector<cl_platform_id> platforms;
-	platforms.resize(numOfPlatforms);
-	clGetPlatformIDs(numOfPlatforms, &platforms[0], nullptr);
+	if (numOfPlatforms == 0)
+	{
+		std::cout << "Could not find OpenCL platform" << std::endl;
+		throw "Could not find OpenCL platform";
+	}
 
-	auto platform = platforms.begin();
-	cl_device_id deviceId;
+	// Gets all available OpenCL platforms
+	std::vector<cl_platform_id> platformIds;
+	platformIds.resize(numOfPlatforms);
+	clGetPlatformIDs(numOfPlatforms, &platformIds[0], nullptr);
 
-	for (; platform != platforms.end(); ++platform) 
+	for (auto platformId = platformIds.begin(); platformId != platformIds.end(); ++platformId)
 	{
 		cl_context_properties properties[] = {
-			CL_GL_CONTEXT_KHR, (cl_context_properties)wglGetCurrentContext(),
-			CL_WGL_HDC_KHR, (cl_context_properties)wglGetCurrentDC(),
-			CL_CONTEXT_PLATFORM, (cl_context_properties)(*platform),
+			CL_GL_CONTEXT_KHR, (cl_context_properties)wglGetCurrentContext(),	// Windows OpenGL context
+			CL_WGL_HDC_KHR, (cl_context_properties)wglGetCurrentDC(),			// Windows OpenGL HDC
+			CL_CONTEXT_PLATFORM, (cl_context_properties)(*platformId),			// OpenCL platform
 			0
 		};
 
+		// Gets pointer to function clGetGLContextInfoKHR
 		clGetGLContextInfoKHR_fn clGetGLContextInfoKHR =
-			(clGetGLContextInfoKHR_fn)clGetExtensionFunctionAddressForPlatform(*platform, "clGetGLContextInfoKHR");
+			(clGetGLContextInfoKHR_fn)clGetExtensionFunctionAddressForPlatform(*platformId, "clGetGLContextInfoKHR");
 
+		cl_device_id deviceId;
+
+		// Gets Id of the device associated with the current OpenGL context
 		cl_int errorCode = clGetGLContextInfoKHR(
 			properties, CL_CURRENT_DEVICE_FOR_GL_CONTEXT_KHR, sizeof(cl_device_id), &deviceId, nullptr);
 
 		if (errorCode == CL_SUCCESS)
 		{
-			print(cl::Platform(*platform));
+			print(cl::Platform(*platformId));
 			print(cl::Device(deviceId));
 
 			device = cl::Device(deviceId);
@@ -53,8 +62,8 @@ void CLContext::init()
 		}
 	}
 
-	std::cout << "Can't find CL/GL compatible device" << std::endl;
-	throw "Can't find CL/GL compatible device";
+	std::cout << "Could not find CL/GL interoperable device" << std::endl;
+	throw "Could not find CL/GL interoperable device";
 }
 
 const cl::Device& CLContext::getDevice() const

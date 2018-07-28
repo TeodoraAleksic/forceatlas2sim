41e30d048275887110b266f9ca0233b73f88725f
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-07-28 20:18:45 +0200

    Break shader lines for error message readability

--- forceatlas2sim/shader.h
+++ forceatlas2sim/shader.h
@@ -8,109 +8,109 @@ namespace shader
 	const std::string nodeVert =
 	" \
 	#version 330 core \n\
-	\
-	layout(location = 0) in vec3 position; \
-	layout(location = 1) in float offsetX; \
-	layout(location = 2) in float offsetY; \
-	layout(location = 3) in float offsetZ; \
-	layout(location = 4) in uint scale; \
-	\
-	uniform mat4 projection; \
-	uniform mat4 view; \
-	uniform mat4 model; \
-	\
-	uniform mat3 normalMatrix;\
-	\
-	out vec3 vertPos;\
-	out vec3 vertNormal;\
-	\
-	void main() \
-	{ \
-		vertPos = vec3(model * vec4(position, 1.0f));\
-		vertNormal = normalMatrix * normalize(position);\
-		\
-		vec3 offset = vec3(offsetX, offsetY, offsetZ); \
-		vec3 newPos = position * (float(scale) * 0.5 + 0.5) + offset; \
-		gl_Position = projection * view * model * vec4(newPos, 1); \
-	} \
+	\n\
+	layout(location = 0) in vec3 position; \n\
+	layout(location = 1) in float offsetX; \n\
+	layout(location = 2) in float offsetY; \n\
+	layout(location = 3) in float offsetZ; \n\
+	layout(location = 4) in uint scale; \n\
+	\n\
+	uniform mat4 projection; \n\
+	uniform mat4 view; \n\
+	uniform mat4 model; \n\
+	\n\
+	uniform mat3 normalMatrix; \n\
+	\n\
+	out vec3 vertPos; \n\
+	out vec3 vertNormal; \n\
+	\n\
+	void main() \n\
+	{ \n\
+		vertPos = vec3(model * vec4(position, 1.0f)); \n\
+		vertNormal = normalMatrix * normalize(position); \n\
+		\n\
+		vec3 offset = vec3(offsetX, offsetY, offsetZ); \n\
+		vec3 newPos = position * (float(scale) * 0.5 + 0.5) + offset; \n\
+		gl_Position = projection * view * model * vec4(newPos, 1); \n\
+	} \n\
 	";
 
 	const std::string nodeFrag =
 	" \
 	#version 330 core \n\
-	\
-	in vec3 vertPos;\
-	in vec3 vertNormal;\
-	\
-	out vec4 outColor; \
-	\
-	void main() { \
-		vec3 lightPos = vec3(50.0, 50.0, 50.0);\
-		vec3 nodeColor = vec3(1.0, 0.0, 1.0);\
-		vec3 lightColor = vec3(1.0, 1.0, 1.0);\
-		\
-		float ambientStrength = 0.2; \
-		vec3 ambient = ambientStrength * lightColor; \
-		\
-		vec3 normal = normalize(vertNormal); \
-		vec3 lightDir = normalize(lightPos - vertPos); \
-		vec3 diffuse = max(dot(normal, lightDir), 0.0) * lightColor; \
-		\
-		vec3 color = (ambient + diffuse) * nodeColor; \
-		outColor = vec4(color, 1.0); \
-	} \
+	\n\
+	in vec3 vertPos; \n\
+	in vec3 vertNormal; \n\
+	\n\
+	out vec4 outColor; \n\
+	\n\
+	void main() { \n\
+		vec3 lightPos = vec3(50.0, 50.0, 50.0); \n\
+		vec3 nodeColor = vec3(1.0, 0.0, 1.0); \n\
+		vec3 lightColor = vec3(1.0, 1.0, 1.0); \n\
+		\n\
+		float ambientStrength = 0.2; \n\
+		vec3 ambient = ambientStrength * lightColor; \n\
+		\n\
+		vec3 normal = normalize(vertNormal); \n\
+		vec3 lightDir = normalize(lightPos - vertPos); \n\
+		vec3 diffuse = max(dot(normal, lightDir), 0.0) * lightColor; \n\
+		\n\
+		vec3 color = (ambient + diffuse) * nodeColor; \n\
+		outColor = vec4(color, 1.0); \n\
+	} \n\
 	";
 
 	const std::string edgeVert =
 	" \
 	#version 330 core \n\
-	\
-	layout(location = 0) in vec3 position; \
-	layout(location = 1) in float sourceX; \
-	layout(location = 2) in float sourceY; \
-	layout(location = 3) in float sourceZ; \
-	layout(location = 4) in float targetX; \
-	layout(location = 5) in float targetY; \
-	layout(location = 6) in float targetZ; \
-	\
-	uniform mat4 projection; \
-	uniform mat4 view; \
-	uniform mat4 model; \
-	\
-	void main() \
-	{ \
-		vec3 source = vec3(sourceX, sourceY, sourceZ); \
-		vec3 target = vec3(targetX, targetY, targetZ); \
-	\
-		float scale = sqrt(pow(source.x - target.x, 2) + pow(source.y - target.y, 2) + pow(source.z - target.z, 2)); \
-		vec3 offset = source - vec3(0.0, 0.0, 0.0); \
-	\
-		vec3 rotationX = target - offset; \
-		vec3 rotationY = normalize(cross(vec3(0.0f, 1.0f, 0.0f), rotationX)); \
-		vec3 rotationZ = cross(rotationX, rotationY); \
-	\
-		rotationX = normalize(rotationX); \
-		rotationY = normalize(rotationY); \
-		rotationZ = normalize(rotationZ); \
-	\
-		mat4 rotation = mat4(vec4(rotationX, 0.0), vec4(rotationY, 0.0), vec4(rotationZ, 0.0), vec4(0.0, 0.0, 0.0, 1.0)); \
-		mat4 translation = mat4(vec4(1.0, 0.0, 0.0, 0.0), vec4(0.0, 1.0, 0.0, 0.0), vec4(0.0, 0.0, 1.0, 0.0), vec4(offset, 1.0)); \
-	\
-		vec4 newPos = translation * rotation * vec4(position.x * scale, position.y, position.z, 1.0); \
-	\
-		gl_Position = projection * view * model * newPos; \
-	} \
+	\n\
+	layout(location = 0) in vec3 position; \n\
+	layout(location = 1) in float sourceX; \n\
+	layout(location = 2) in float sourceY; \n\
+	layout(location = 3) in float sourceZ; \n\
+	layout(location = 4) in float targetX; \n\
+	layout(location = 5) in float targetY; \n\
+	layout(location = 6) in float targetZ; \n\
+	\n\
+	uniform mat4 projection; \n\
+	uniform mat4 view; \n\
+	uniform mat4 model; \n\
+	\n\
+	void main() \n\
+	{ \n\
+		vec3 source = vec3(sourceX, sourceY, sourceZ); \n\
+		vec3 target = vec3(targetX, targetY, targetZ); \n\
+		\n\
+		float scale = sqrt(pow(source.x - target.x, 2) + pow(source.y - target.y, 2) + pow(source.z - target.z, 2)); \n\
+		vec3 offset = source - vec3(0.0, 0.0, 0.0); \n\
+		\n\
+		vec3 rotationX = target - offset; \n\
+		vec3 rotationY = normalize(cross(vec3(0.0f, 1.0f, 0.0f), rotationX)); \n\
+		vec3 rotationZ = cross(rotationX, rotationY); \n\
+		\n\
+		rotationX = normalize(rotationX); \n\
+		rotationY = normalize(rotationY); \n\
+		rotationZ = normalize(rotationZ); \n\
+		\n\
+		mat4 rotation = mat4(vec4(rotationX, 0.0), vec4(rotationY, 0.0), vec4(rotationZ, 0.0), vec4(0.0, 0.0, 0.0, 1.0)); \n\
+		mat4 translation = mat4(vec4(1.0, 0.0, 0.0, 0.0), vec4(0.0, 1.0, 0.0, 0.0), vec4(0.0, 0.0, 1.0, 0.0), vec4(offset, 1.0)); \n\
+		\n\
+		vec4 newPos = translation * rotation * vec4(position.x * scale, position.y, position.z, 1.0); \n\
+		\n\
+		gl_Position = projection * view * model * newPos; \n\
+	} \n\
 	";
 
 	const std::string edgeFrag =
 	" \
 	#version 330 core \n\
-	\
+	\n\
 	out vec4 outColor; \
-	\
-	void main() { \
-		outColor = vec4(0.85, 0.85, 0.85, 1.0); \
-	} \
+	\n\
+	void main() { \n\
+		outColor = vec4(0.85, 0.85, 0.85, 1.0); \n\
+	} \n\
 	";
 }
 

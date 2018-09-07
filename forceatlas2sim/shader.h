1edcba20343d3605ee6536b416cc3574d3876a0c
Teodora Aleksic <teodora_aleksic@ymail.com>
2018-09-07 21:50:48 +0200

    Thicken edges of selected node

--- forceatlas2sim/shader.h
+++ forceatlas2sim/shader.h
@@ -94,6 +94,7 @@ namespace shader
 	uniform mat4 projection; \n\
 	uniform mat4 view; \n\
 	uniform mat4 model; \n\
+	uniform uint selectedNode; \n\
 	\n\
 	flat out uint edgeId1; \n\
 	flat out uint edgeId2; \n\
@@ -103,10 +104,12 @@ namespace shader
 		edgeId1 = sourceId; \n\
 		edgeId2 = targetId; \n\
 		\n\
+		int thickness = (edgeId1 == selectedNode || edgeId2 == selectedNode) ? 3 : 1;\n\
+		\n\
 		vec3 source = vec3(sourceX, sourceY, sourceZ); \n\
 		vec3 target = vec3(targetX, targetY, targetZ); \n\
 		\n\
-		float scale = sqrt(pow(source.x - target.x, 2) + pow(source.y - target.y, 2) + pow(source.z - target.z, 2)); \n\
+		float length = sqrt(pow(source.x - target.x, 2) + pow(source.y - target.y, 2) + pow(source.z - target.z, 2)); \n\
 		vec3 offset = source - vec3(0.0, 0.0, 0.0); \n\
 		\n\
 		vec3 rotationX = target - offset; \n\
@@ -120,7 +123,7 @@ namespace shader
 		mat4 rotation = mat4(vec4(rotationX, 0.0), vec4(rotationY, 0.0), vec4(rotationZ, 0.0), vec4(0.0, 0.0, 0.0, 1.0)); \n\
 		mat4 translation = mat4(vec4(1.0, 0.0, 0.0, 0.0), vec4(0.0, 1.0, 0.0, 0.0), vec4(0.0, 0.0, 1.0, 0.0), vec4(offset, 1.0)); \n\
 		\n\
-		vec4 newPos = translation * rotation * vec4(position.x * scale, position.y, position.z, 1.0); \n\
+		vec4 newPos = translation * rotation * vec4(position.x * length, position.y * thickness, position.z * thickness, 1.0); \n\
 		\n\
 		gl_Position = projection * view * model * newPos; \n\
 	} \n\
